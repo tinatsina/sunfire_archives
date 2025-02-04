59b1113f0d64e3871963be08c371d9d10d631544
tinayetsina <tinayetsina@outlook.com>
2025-02-04 15:09:32 +0100

    update - champion dictionary created

--- lol_context.py
+++ lol_context.py
@@ -7,3 +7,30 @@ class Champion:
     CHAMPION_STATS = [-1,-1,-1,-1,]
     SCALING_POTENTIAL = -1
     CORE_IS_BUILT = -1
+
+
+    x = ['Fighter', 'Marksman', 'Tank', 'Assassin', 'Support', 'Mage']
+class Team:
+    BLUE_TEAM_FIGHTER_COUNT = -1
+    BLUE_TEAM_MARKSMAN_COUNT = -1
+    BLUE_TEAM_TANK_COUNT = -1
+    BLUE_TEAM_ASSASIN_COUNT = -1
+    BLUE_TEAM_SUPPORT_COUNT = -1
+    BLUE_TEAM_MAGE_COUNT = -1
+
+    RED_TEAM_FIGHTER_COUNT = -1
+    RED_TEAM_MARKSMAN_COUNT = -1
+    RED_TEAM_TANK_COUNT = -1
+    RED_TEAM_ASSASIN_COUNT = -1
+    RED_TEAM_SUPPORT_COUNT = -1
+    RED_TEAM_MAGE_COUNT = -1
+
+    BLUE_TEAM_CC = -1
+    RED_TEAM_CC = -1
+
+    BLUE_TEAM_PP = -1
+    RED_TEAM_PP = -1
+
+    BLUE_TEAM_SK = -1
+    RED_TEAM_SK = -1
+    
