4252a6defa97f3135d7514197a626cff4b5a3386
tinayetsina <tinayetsina@outlook.com>
2025-02-04 13:39:51 +0100

    update - data cleaning complet

---
+++ lol_context.py
@@ -0,0 +1,9 @@
+class Champion:
+    NAME = ""
+    ID = -1
+    DELTA = -1
+    WIN_RATE = -1
+    CHAMPION_TYPE = [-1,-1,-1,-1,-1]
+    CHAMPION_STATS = [-1,-1,-1,-1,]
+    SCALING_POTENTIAL = -1
+    CORE_IS_BUILT = -1
